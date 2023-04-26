acfdc42aa15f406fe39d8653c095449a2d303f65
vanrest.remo <vanrest.remo@gmail.com>
2023-04-26 17:50:10 +0200

    correctie voor graphQL zie issue #1

--- Indicatieregister/Zorgkantoor.lua
+++ Indicatieregister/Zorgkantoor.lua
@@ -1,4 +1,4 @@
--- versie 20230317
+-- versie 20230426
 function handle(eventData)
     local jsonModelScratch = [[
       {
@@ -8,8 +8,8 @@ function handle(eventData)
           }
         },
         "W": {
-          "m": { "grondslag": "#G", "geindiceerdZorgzwaartepakket": "#GZ", "geindiceerdeFunctie": "#GF", "beperking": "#B", "stoornis": "#S", "stoornisScore": "#SS", "wzd": "#WZD", "client": "#C" },
-          "f": ["wlzindicatieID", "bsn", "besluitnummer", "soortWlzindicatie", "afgiftedatum", "ingangsdatum", "einddatum", "wzdVerklaringBijAfgifte", "meerzorg", "commentaar"],
+          "m": { "grondslag": "#G", "geindiceerdZorgzwaartepakket": "#GZ", "beperking": "#B", "stoornis": "#S", "stoornisScore": "#SS", "wzd": "#WZD", "client": "#C" },
+          "f": ["wlzindicatieID", "bsn", "besluitnummer", "soortWlzindicatie", "afgiftedatum", "ingangsdatum", "einddatum", "meerzorg", "commentaar"],
           "p": {
             "wlzindicatieID": "$$ReplaceWithRecordID$$"
           }
@@ -18,10 +18,7 @@ function handle(eventData)
           "f" : ["id", "grondslag", "volgorde", "wlzindicatieID"]
         },
         "GZ": {
-          "f" : ["id", "zzpCode", "ingangsdatum", "einddatum", "klasse", "voorkeurClient", "instellingVoorkeur", "financiering", "commentaar", "wlzindicatieID"]
-        },
-        "GF": {
-          "f": ["id", "functiecode", "ingangsdatum", "einddatum", "klasse", "opslag", "leveringsVoorwaarde", "vervoer", "instellingVoorkeur", "financiering", "commentaar", "wlzindicatieID"]
+          "f" : ["id", "zzpCode", "ingangsdatum", "einddatum", "voorkeurClient", "instellingVoorkeur", "financiering", "commentaar", "wlzindicatieID"]
         },
         "B": {
           "m": {"beperkingScores":  "#BS"},
